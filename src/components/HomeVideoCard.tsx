ba4c545600a37b11661bfdf15d219c18a4571759
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2024-01-09 12:30:21 +0100

    improve home miniatures display

--- src/components/HomeVideoCard.tsx
+++ src/components/HomeVideoCard.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useRef} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import {Typography} from '@mui/material';
 
 import type Recommendation from '../common/types/Recommendation';
@@ -19,111 +19,133 @@ export const HomeVideoCard: React.FC<Recommendation & {
 }) => {
 	const [isHovering, setHovering] = useState(false);
 
-	const mediaLink = isHovering && hoverAnimationUrl
-		? hoverAnimationUrl
-		: miniatureUrl;
-
 	const imgRef = useRef<HTMLImageElement>(null);
 
-	return (<section
-		className='style-scope ytd-rich-item-renderer'
-		onMouseEnter={() => {
-			setHovering(true);
-		}}
-		onMouseLeave={() => {
-			setHovering(false);
-		}}
-	>
-		<div
-			id='thumbnail'
-			className='style-scope ytd-rich-grid-media'
-			onClick={onClick}
-		>
-			<div className='style-scope ytd-rich-grid-media'>
-				<a
-					className='yt-simple-endpoint inline-block style-scope ytd-thumbnail'
-					href={`/watch?v=${videoId}`}
-				>
-					<div className='style-scope ytd-thumbnail'>
-						{(!isHovering && <img
-							style={{
-								backgroundColor: 'transparent',
-								borderRadius: 12,
-							}}
-							className='yt-core-image--fill-parent-height yt-core-image--fill-parent-width yt-core-image yt-core-image--content-mode-scale-aspect-fill yt-core-image--loaded'
-							src={mediaLink}
-							ref={imgRef}
-						/>)}
-						<img
-							style={{
-								backgroundColor: 'transparent',
-								borderRadius: 12,
-								display: isHovering ? 'block' : 'none',
-								...(imgRef.current ? {
-									width: imgRef.current.width,
-									height: imgRef.current.height,
-								} : {}),
-							}}
-							className='yt-core-image--fill-parent-height yt-core-image--fill-parent-width yt-core-image yt-core-image--content-mode-scale-aspect-fill yt-core-image--loaded'
-							src={mediaLink}
-						/>
-					</div>
-				</a>
-			</div>
-		</div>
-		<div id='details' className='style-scope ytd-rich-grid-media'>
-			<a
-				id='avatar-link'
-				href={channelShortName}
-				title={channelName}
-				className='yt-simple-endpoint style-scope ytd-rich-grid-media'
+	const candidateMiniatureUrls = [
+		`https://i.ytimg.com/vi/${videoId}/hq720.jpg`,
+		miniatureUrl,
+	];
+
+	const [pictureUrl, setPictureUrl] = useState<string | undefined>(candidateMiniatureUrls[0]);
+
+	const mediaLink = isHovering && hoverAnimationUrl
+		? hoverAnimationUrl
+		: pictureUrl;
+
+	useEffect(() => {
+		(async () => {
+			const res = await fetch(candidateMiniatureUrls[0], {
+				method: 'HEAD',
+			});
+
+			if (!res.ok) {
+				setPictureUrl(candidateMiniatureUrls[1]);
+			}
+		})().catch(console.error);
+	});
+
+	return (
+		<section className='style-scope ytd-rich-grid-row' onClick={onClick}>
+			<div
+				className='style-scope ytd-rich-item-renderer'
+				onMouseEnter={() => {
+					setHovering(true);
+				}}
+				onMouseLeave={() => {
+					setHovering(false);
+				}}
 			>
-				<div className='style-scope ytd-rich-grid-media no-transition'>
-					<img
-						width={48}
-						className='style-scope yt-img-shadow'
-						src={channelMiniatureUrl}
-						style={{borderRadius: '50%'}}
-					/>
+				<div
+					id='thumbnail'
+					className='style-scope ytd-rich-grid-media'
+				>
+					<div className='style-scope ytd-rich-grid-media'>
+						<a
+							className='yt-simple-endpoint inline-block style-scope ytd-thumbnail'
+							href={`/watch?v=${videoId}`}
+						>
+							<div className='style-scope ytd-thumbnail'>
+								{(!isHovering && <img
+									style={{
+										backgroundColor: 'transparent',
+										borderRadius: 12,
+									}}
+									className='yt-core-image--fill-parent-height yt-core-image--fill-parent-width yt-core-image yt-core-image--content-mode-scale-aspect-fill yt-core-image--loaded'
+									src={mediaLink}
+									ref={imgRef}
+								/>)}
+								<img
+									style={{
+										backgroundColor: 'transparent',
+										borderRadius: 12,
+										display: isHovering ? 'block' : 'none',
+										...(imgRef.current ? {
+											width: imgRef.current.width,
+											height: imgRef.current.height,
+										} : {}),
+									}}
+									className='yt-core-image--fill-parent-height yt-core-image--fill-parent-width yt-core-image yt-core-image--content-mode-scale-aspect-fill yt-core-image--loaded'
+									src={mediaLink}
+								/>
+							</div>
+						</a>
+					</div>
 				</div>
-			</a>
-			<div id='meta' className='style-scope ytd-rich-grid-media'>
-				<h3 className='style-scope ytd-rich-grid-media'>
+				<div id='details' className='style-scope ytd-rich-grid-media'>
 					<a
-						className='yt-simple-endpoint focus-on-expand style-scope ytd-rich-grid-media'
-						href={`/watch?v=${videoId}`}
-						title={title}
+						id='avatar-link'
+						href={channelShortName}
+						title={channelName}
+						className='yt-simple-endpoint style-scope ytd-rich-grid-media'
 					>
-						<div className='style-scope ytd-rich-grid-media'>
-							<Typography variant='h3' component='h3'>
-								{title}
-							</Typography>
+						<div className='style-scope ytd-rich-grid-media no-transition'>
+							<img
+								width={48}
+								className='style-scope yt-img-shadow'
+								src={channelMiniatureUrl}
+								style={{borderRadius: '50%'}}
+							/>
 						</div>
 					</a>
-				</h3>
-				<div className='grid style-scope ytd-rich-grid-media byline-separated'>
-					<div id='metadata' className='style-scope ytd-video-meta-block' style={{
-						flexDirection: 'column',
-					}}>
-						<div id='byline-container' className='style-scope ytd-video-meta-block'>
-							<div className='style-scope ytd-video-meta-block'>
-								<div id='container' className='style-scope ytd-channel-name'>
-									<Typography variant='body2' component='a'>
-										{channelName}
+					<div id='meta' className='style-scope ytd-rich-grid-media'>
+						<h3 className='style-scope ytd-rich-grid-media'>
+							<a
+								className='yt-simple-endpoint focus-on-expand style-scope ytd-rich-grid-media'
+								href={`/watch?v=${videoId}`}
+								title={title}
+							>
+								<div className='style-scope ytd-rich-grid-media'>
+									<Typography variant='h3' component='h3'>
+										{title}
+									</Typography>
+								</div>
+							</a>
+						</h3>
+						<div className='grid style-scope ytd-rich-grid-media byline-separated'>
+							<div id='metadata' className='style-scope ytd-video-meta-block' style={{
+								flexDirection: 'column',
+							}}>
+								<div id='byline-container' className='style-scope ytd-video-meta-block'>
+									<div className='style-scope ytd-video-meta-block'>
+										<div id='container' className='style-scope ytd-channel-name'>
+											<Typography variant='body2' component='a'>
+												{channelName}
+											</Typography>
+										</div>
+									</div>
+								</div>
+								<div id='metadata-line' className='style-scope ytd-video-meta-block'>
+									<Typography variant='body2' component='span'>
+										{views}&nbsp;•&nbsp;{publishedSince}
 									</Typography>
 								</div>
 							</div>
 						</div>
-						<div id='metadata-line' className='style-scope ytd-video-meta-block'>
-							<Typography variant='body2' component='span'>
-								{views}&nbsp;•&nbsp;{publishedSince}
-							</Typography>
-						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	</section>);
+		</section>
+	);
 };
 
 export default HomeVideoCard;
