61022809c26e365c1caf961d233e2e9ee202331c
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2024-03-03 12:29:00 +0100

    make login modal closable

--- src/subApp/loginApp.tsx
+++ src/subApp/loginApp.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {createRoot} from 'react-dom/client';
 
 import {
 	Box,
+	IconButton,
 	Typography,
 } from '@mui/material';
+
+import {Close as CloseIcon} from '@mui/icons-material';
 
 import {getThemeContrastBackgroundColor, getThemeContrastTextColor} from '../theme';
 
 import {type SubAppCreator, type SubAppState, ReactAdapter} from '../SubApp';
 
 const LoginApp: React.FC<SubAppState> = ({loggedInExtension, loggedInYouTube}) => {
+	const [shown, setShown] = useState(true);
+
 	if (loggedInExtension && loggedInYouTube) {
+		return null;
+	}
+
+	if (!shown) {
 		return null;
 	}
 
@@ -21,13 +30,29 @@ const LoginApp: React.FC<SubAppState> = ({loggedInExtension, loggedInYouTube}) =
 				position: 'fixed',
 				top: '50%',
 				left: '50%',
-				transform: 'translate(-50%, -50%)',
+				transform: 'translate(-50%, -75%)',
 				zIndex: 10000,
 				backgroundColor: getThemeContrastBackgroundColor(),
 				color: getThemeContrastTextColor(),
 				padding: 4,
 				borderRadius: 4,
 			}}>
+				<Box sx={{textAlign: 'right'}}>
+					<IconButton
+						size='large'
+						onClick={() => {
+							setShown(false);
+							setTimeout(() => {
+								setShown(true);
+							}, 1000 * 60 * 5);
+						}}
+						sx={{
+							color: getThemeContrastTextColor(),
+						}}
+					>
+						<CloseIcon />
+					</IconButton>
+				</Box>
 				<Typography variant='h4' sx={{m: 2, color: getThemeContrastTextColor()}}>
 					Welcome to the YouTube Experiment extension!
 				</Typography>
